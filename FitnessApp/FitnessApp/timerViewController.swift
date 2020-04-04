4d4813afa143771067d885cf8fde3b661ea8f716
codeplus <codeplus@CDSS-4248.attlocal.net>
2020-04-04 16:15:45 -0400

    timer buttons

--- FitnessApp/FitnessApp/timerViewController.swift
+++ FitnessApp/FitnessApp/timerViewController.swift
@@ -13,31 +13,62 @@ import UIKit
 class timerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
 
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var startButton: UIButton!
+    
+    @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var timerTextField: UITextField!
     @IBOutlet weak var progressTextField: UITextField!
+    
+    
     
     let timerList = UIPickerView()
     let progressList = UIPickerView()
     
+    
+    var timer: Timer?
+    var totalTime: Int = 0
+
+
+    @objc func updateTimer() {
+        print(self.totalTime)
+        self.timerLabel.text = self.timeFormatted(self.totalTime) // will show timer
+        if totalTime != 0 {
+            totalTime -= 1  // decrease counter timer
+        } else {
+            if let timer = self.timer {
+                timer.invalidate()
+                self.timer = nil
+            }
+        }
+    }
+    
+    func timeFormatted(_ totalSeconds: Int) -> String {
+        let seconds: Int = totalSeconds % 60
+        let minutes: Int = (totalSeconds / 60) % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    
+    
+    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
            return 1
     }
-       
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return 60
     }
-       
     // This function sets the text of the picker view to row# + 1
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return String(row+1) + " min"
     }
-    
     // When user selects an option, this function will set the text of the text field to reflect the selected option.
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         timerTextField.text = String(row+1) + " min"
+        totalTime = row + 1
         //progressTextField.text = String(row+1) + " min"
     }
-    
     @objc func donePicker() {
         timerTextField.resignFirstResponder()
         progressTextField.resignFirstResponder()
@@ -55,6 +86,8 @@ class timerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         pickerTextField.rightView = arrow
         pickerTextField.rightViewMode = UITextField.ViewMode.always
         */
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: false)
         
         // Make done button
         let toolBar = UIToolbar()
@@ -74,13 +107,36 @@ class timerViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         timerList.dataSource = self
         progressList.delegate = self
         progressList.dataSource = self
+        
+        // Change appearance of start button
+        
+        startButton.layer.shadowOpacity = 0.5
+        startButton.layer.shadowRadius = 1
+        startButton.layer.cornerRadius = 30
+        startButton.layer.borderWidth = 1
+        startButton.layer.borderColor = UIColor.black.cgColor
+        startButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        startButton.layer.masksToBounds = false
+        
+        pauseButton.layer.shadowOpacity = 0.5
+        pauseButton.layer.shadowRadius = 1
+        pauseButton.layer.cornerRadius = 30
+        pauseButton.layer.borderWidth = 1
+        pauseButton.layer.borderColor = UIColor.black.cgColor
+        pauseButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        pauseButton.layer.masksToBounds = false
+        
+        resetButton.layer.shadowOpacity = 0.5
+        resetButton.layer.shadowRadius = 1
+        resetButton.layer.cornerRadius = 30
+        resetButton.layer.borderWidth = 1
+        resetButton.layer.borderColor = UIColor.black.cgColor
+        resetButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        resetButton.layer.masksToBounds = false
     }
     
     
-    
-      
-    
-    
+
     /*
     // MARK: - Navigation
 
