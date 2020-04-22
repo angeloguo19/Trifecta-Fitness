8e03ce43d83f4715c5d8d5c0ebd5e83afbaabb1c
Oliver Rodas <oliver.rodas@duke.edu>
2020-04-21 21:11:16 -0700

    Chart Demo

--- FitnessApp/FitnessApp/meditationViewController.swift
+++ FitnessApp/FitnessApp/meditationViewController.swift
@@ -16,13 +16,13 @@ class meditationViewController: UIViewController {
     @IBOutlet weak var averagetimeLabel: UILabel!
     @IBOutlet weak var newSessionButton: UIButton!
     @IBOutlet weak var moreInfoButton: UIButton!
-    @IBOutlet weak var chartView: LineChartView!
+    @IBOutlet weak var chartView: BarChartView!
     
     
     
     var average: String = ""
-    var pastWeekTimes: [Int] = [0,0,0,0,0,0,0]
-    var days = [NSDate]()
+    var pastWeekTimes: [Int] = [25,15,60,5,30,15,0]
+    var days = [String]()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -49,68 +49,99 @@ class meditationViewController: UIViewController {
         // MARK: Core Data Call
         
         // Get NSDate for past 7 days
-        let calendar = Calendar(identifier: .gregorian)
-        let units: Set<Calendar.Component> = [.year, .month, .day]
-        let components = calendar.dateComponents(units, from: Date())
-        let date: NSDate = calendar.date(from: components)! as NSDate
-        days.append(date)
-        for i in 1 ... 6 {
-            let day = Calendar.current.date(byAdding: .day, value: -i, to: date as Date)!
-            days.append(day as NSDate)
-        }
-        
-        // Retrieve Session entity's and update pastWeekTimes array
-        var results: [NSManagedObject] = []
-        let requests = NSFetchRequest<NSFetchRequestResult>(entityName: "Session")
-        requests.returnsObjectsAsFaults = false
-        do {
-            let fetchResult = try context.fetch(requests)
-            results = fetchResult as! [NSManagedObject]
-            for result in results {
-                let tempdate: NSDate = result.value(forKey: "date") as! NSDate
-                let temptime = result.value(forKey: "time") as! Int
-                var isIn: Int = 0
-                for n in 0 ... 6 {
-                    if tempdate == days[n] {
-                        pastWeekTimes[n] = temptime
-                        isIn = 1
-                    }
-                }
-                if isIn == 0 {
-                    context.delete(result)
-                    do {
-                        try context.save()
-                    } catch let error as NSError {
-                        print("Could not save. \(error), \(error.userInfo)")
-                    }
-                }
-            }
-        } catch let error as NSError {
-            print("Could not fetch. \(error), \(error.userInfo)")
-        }
+//        let calendar = Calendar(identifier: .gregorian)
+//        let units: Set<Calendar.Component> = [.year, .month, .day]
+//        let components = calendar.dateComponents(units, from: Date())
+//        let date: NSDate = calendar.date(from: components)! as NSDate
+//        days.append(date)
+//        for i in 1 ... 6 {
+//            let day = Calendar.current.date(byAdding: .day, value: -i, to: date as Date)!
+//            days.append(day as NSDate)
+//        }
+//
+//        // Retrieve Session entity's and update pastWeekTimes array
+//        var results: [NSManagedObject] = []
+//        let requests = NSFetchRequest<NSFetchRequestResult>(entityName: "Session")
+//        requests.returnsObjectsAsFaults = false
+//        do {
+//            let fetchResult = try context.fetch(requests)
+//            results = fetchResult as! [NSManagedObject]
+//            for result in results {
+//                let tempdate: NSDate = result.value(forKey: "date") as! NSDate
+//                let temptime = result.value(forKey: "time") as! Int
+//                var isIn: Int = 0
+//                for n in 0 ... 6 {
+//                    if tempdate == days[n] {
+//                        pastWeekTimes[n] = temptime
+//                        isIn = 1
+//                    }
+//                }
+//                if isIn == 0 {
+//                    context.delete(result)
+//                    do {
+//                        try context.save()
+//                    } catch let error as NSError {
+//                        print("Could not save. \(error), \(error.userInfo)")
+//                    }
+//                }
+//            }
+//        } catch let error as NSError {
+//            print("Could not fetch. \(error), \(error.userInfo)")
+//        }
+//
+//        // Make Line Graph
+//        var lineChartEntry = [ChartDataEntry]()
+//        for i in 0...6 {
+//            lineChartEntry.append(ChartDataEntry.init(x: Double(i+1), y: Double(pastWeekTimes[i])))
+//        }
+//        let values = LineChartDataSet(entries: lineChartEntry, label: "Time Spent Meditating")
+//        values.colors = [NSUIColor.blue]
+//        let data = LineChartData()
+//        data.addDataSet(values)
+//        chartView.data = data
+//        // Customize Line Graph
+//        chartView.xAxis.labelPosition = .bottom
+//        chartView.leftAxis.axisMinimum = 0.0
+//        chartView.rightAxis.enabled = false
+//        //chartView.chartDescription?.text = "Meditation time over past week"
     
-        // Make Line Graph
-        var lineChartEntry = [ChartDataEntry]()
-        for i in 0...6 {
-            lineChartEntry.append(ChartDataEntry.init(x: Double(i+1), y: Double(pastWeekTimes[i])))
+        //BarChartDataEntry(value)
+    
+    }
+    func setChart(values: [Double]) {
+        chartView.noDataText = "You need to provide data for the chart."
+        let formato:BarChartFormatter = BarChartFormatter()
+        let xAxis = XAxis()
+        var dataEntries: [BarChartDataEntry] = []
+        for i in 0..<values.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), y: values[i])
+            dataEntries.append(dataEntry)
+            //formato.stringForValue(Double(i), axis: xAxis)
         }
-        let values = LineChartDataSet(entries: lineChartEntry, label: "Time Spent Meditating")
-        values.colors = [NSUIColor.blue]
-        let data = LineChartData()
-        data.addDataSet(values)
-        chartView.data = data
-        // Customize Line Graph
+        xAxis.valueFormatter = formato
+        chartView.xAxis.valueFormatter = xAxis.valueFormatter
         chartView.xAxis.labelPosition = .bottom
-        chartView.leftAxis.axisMinimum = 0.0
-        chartView.rightAxis.enabled = false
-        //chartView.chartDescription?.text = "Meditation time over past week"
+        let chartDataSet = BarChartDataSet(dataEntries)
+        let chartData = BarChartData(dataSet: chartDataSet)
+        chartView.data = chartData
+    }
     
-    
+    public class BarChartFormatter: NSObject, IAxisValueFormatter {
+        var days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"]
+        
+        public func stringForValue(_ value: Double, axis: AxisBase?) -> String {
+            return(days[Int(value)])
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        let unitsSold:[Double] = [5, 15, 10, 30, 60, 10, 20]
+                
+        setChart(values: unitsSold)
+        
         // UI for ViewController
         navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
         navigationController?.navigationBar.shadowImage = UIImage()
